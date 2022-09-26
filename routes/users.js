820068731534b8999464dabe238a38784562e85e
Guilherme-Verrange <verrangeguilherme@gmail.com>
2022-09-26 12:56:39 -0300

    Creating a data persistence together with NeDatabase

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,37 @@
+let neDB = require("nedb");
+
+let db = new neDB({
+  filename: "users.db",
+  autoload: true,
+});
+
 module.exports = (app) => {
+  app.get("/users", (req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+      JSON.stringify({
+        users: [
+          {
+            name: "Guilherme Verrange",
+            email: "verrangeguilherme@gmail.com",
+            id: 1,
+          },
+        ],
+      })
+    );
+  });
 
-    app.get('/users',  (req, res) => {
-
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify({
-            users:[{
-    
-                name: 'Guilherme Verrange',
-                email: 'verrangeguilherme@gmail.com',
-                id: 1
-            }]
-    
-        }));
+  app.post("/users", (req, res) => {
+    db.insert(req.body, (err, user) => {
+      if (err) {
+        console.log(`error: ${err}`);
+        res.status(400).json({
+          error: err,
+        });
+      } else {
+        res.status(200).json(user);
+      }
     });
-    
-    
-    app.post('/users', (req, res) =>{
-    
-       
-        res.json(req.body);
-    
-    });
-
-}
+  });
+};
