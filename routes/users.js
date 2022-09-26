6357fb5ff9e061f66f97c7713eb0c30a051a3f51
Guilherme-Verrange <verrangeguilherme@gmail.com>
2022-09-26 12:19:03 -0300

    Getting the data through POST and working with Postman

--- routes/users.js
+++ routes/users.js
@@ -16,19 +16,10 @@ module.exports = (app) => {
     });
     
     
-    app.get('/users/admin', (req, res) =>{
+    app.post('/users', (req, res) =>{
     
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify({
-            users:[{
-    
-                name: 'admin',
-                email: 'admin@gmail.com',
-                id: 2
-            }]
-    
-        }));
+       
+        res.json(req.body);
     
     });
 
