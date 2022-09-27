52527e066b2c2b32137b5af1e3b208dbeb0fd249
Guilherme-Verrange <verrangeguilherme@gmail.com>
2022-09-27 08:38:27 -0300

    Listing NeDB users sorted by name.

--- routes/users.js
+++ routes/users.js
@@ -7,19 +7,23 @@ let db = new neDB({
 
 module.exports = (app) => {
   app.get("/users", (req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.end(
-      JSON.stringify({
-        users: [
-          {
-            name: "Guilherme Verrange",
-            email: "verrangeguilherme@gmail.com",
-            id: 1,
-          },
-        ],
-      })
-    );
+
+    db.find({}).sort({name:1}).exec((err, users)=>{ // Filtra o usuário 
+
+      if(err){
+        console.log(`error: ${err}`);
+        res.status(400).json({ //Verificação caso ocorre um erro ele ser exibido na tela
+          error:err
+        })
+      }else{
+
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.json({
+          users //Faz um get e imprimi os usuários
+        });
+      }
+    });
   });
 
   app.post("/users", (req, res) => {
